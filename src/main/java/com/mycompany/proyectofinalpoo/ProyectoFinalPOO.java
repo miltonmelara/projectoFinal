6d677d8aa22c000863d9db85219f729c7883b7e0
Milton Melara <milton.melara3b@gmail.com>
2025-08-28 14:10:06 -0600

    First commit

---
+++ src/main/java/com/mycompany/proyectofinalpoo/ProyectoFinalPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinalpoo;
+
+/**
+ *
+ * @author Bebe
+ */
+public class ProyectoFinalPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
